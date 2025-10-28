2f4ca1de2cedf53c1256767f5c25b8a41bfb4509
Mohamed Aymen Elmezouari <yeezyuus@Mo-MacBook-Pro.local>
2025-10-28 23:43:56 +0100

    feat: add normalize_unicode function and tests

--- README.md
+++
@@ -1,8 +0,0 @@
-# textutils-Team8
-Team 8 - Python for Data Science, Term 1
-
-- Mara Rusen
-- Abhay Mathahalli
-- Mohamed Aymen Elmezouari
-- Ferdinand Rasmussen
-- Amat Montoto
--- tests/unit/test_remove_punctuation.py
+++
@@ -1,9 +0,0 @@
-from textutils.core import remove_punctuation
-
-def test_remove_basic():
-    assert remove_punctuation("Hello, world!") == "Hello world"
-    assert remove_punctuation("Hi... I'm Miba!") == "Hi Im Miba"
-
-def test_remove_edge():
-    assert remove_punctuation("123 ? !") == "123  "
-    assert remove_punctuation("") == ""
